31266e1fb9220c225d02a00dfcb99eca6e314e41
James Hawk <jph94880@gmail.com>
2023-09-06 21:51:38 -0500

    Adds my file

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,19 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+        response = requests.get(URL)
+        return response.content
 
     def load_json(self):
-        pass
+        people_list = []
+        people = json.loads(self.get_response_body())
+        for person in people:
+            people_list.append(person)
+        return people_list
+    
+get_requester = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+get_requester.load_json()
+
+# for person in set(name_and_job):
+#     print(person)
